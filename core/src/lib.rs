48898629ef945345e8a3601c8fa28bed906ff5b0
Vladimír Marcin <marcinvlado@gmail.com>
2023-04-01 14:38:26 +0100

    :bug: Format/edition changes

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,6 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Guess {
-    pub secret_giuess: u64,
+    pub secret_guess: u64,
     pub secret_choice: u64,
 }
